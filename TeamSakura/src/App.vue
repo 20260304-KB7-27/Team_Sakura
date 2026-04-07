f2b25c11508a481ccd08c9ca96fc27c9d64c2cfb
dony-beep <zone0728@gmail.com>
2026-04-07 09:18:39 +0900

    feat: 메인 페이지 제작

--- TeamSakura/src/App.vue
+++ TeamSakura/src/App.vue
@@ -1,6 +1,10 @@
-<script setup></script>
+<script setup>
+import Header from './home/Header.vue';
+
+</script>
 
 <template>
+  <Header></Header>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
---
+++ TeamSakura/src/home/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand bg-dark navbar-dark px-3">
+    <span class="navbar-brand">🌸{{ teamName.name }}🌸</span>
+
+    <div class="navbar-nav-container">
+      <ul class="navbar-nav flex-row">
+        <li class="nav-item px-2">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item px-2">
+          <router-link class="nav-link" to="/member">Member</router-link>
+        </li>
+        <li class="nav-item px-2">
+          <router-link class="nav-link" to="/team">Team</router-link>
+        </li>
+        <li class="nav-item px-2">
+          <router-link class="nav-link" to="/rule">Rule</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const teamName = ref('');
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/teamName');
+    teamName.value = response.data[0];
+  } catch (error) {
+    console.log('[에러] 팀명 데이터를 가져오는데 실패했습니다 : ', error);
+  }
+});
+</script>
